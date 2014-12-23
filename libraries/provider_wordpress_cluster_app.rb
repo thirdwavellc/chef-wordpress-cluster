7b34d48be96332dd13eb0b1c5196de513b0901a4
Adam Krone <krone.adam@gmail.com>
2014-12-23 16:44:56 -0600

    WIP - Adding development environment support

--- libraries/provider_wordpress_cluster_app.rb
+++ libraries/provider_wordpress_cluster_app.rb
@@ -11,43 +11,47 @@ class Chef
       end
 
       action :create do
-        capistrano_user 'deploy' do
-          group 'deploy'
-          group_id 3000
-        end
+        unless new_resource.development
+          capistrano_user 'deploy' do
+            group 'deploy'
+            group_id 3000
+          end
 
-        include_recipe 'capistrano-base::ssh'
+          include_recipe 'capistrano-base::ssh'
+        end
 
         capistrano_wordpress_app new_resource.app_name do
           deploy_root "/var/www/#{new_resource.app_name}"
           docroot "/var/www/#{new_resource.app_name}/current/web"
-          deployment_user 'deploy'
-          deployment_group 'deploy'
+          deployment_user new_resource.deployment_user
+          deployment_group new_resource.deployment_group
           server_name new_resource.server_name
         end
 
-        capistrano_shared_file '.env.ctmpl' do
-          template '.env.ctmpl.erb'
-          deploy_root "/var/www/#{new_resource.app_name}"
-          owner 'deploy'
-          group 'deploy'
-        end
+        unless new_resource.development
+          capistrano_shared_file '.env.ctmpl' do
+            template '.env.ctmpl.erb'
+            deploy_root "/var/www/#{new_resource.app_name}"
+            owner new_resource.deployment_user
+            group new_resource.deployment_group
+          end
 
-        include_recipe 'consul::default'
-        include_recipe 'consul-services::apache2'
-        include_recipe 'consul-services::consul-template'
+          include_recipe 'consul::default'
+          include_recipe 'consul-services::apache2'
+          include_recipe 'consul-services::consul-template'
 
-        node.normal['consul_template'] = {
-          consul: '127.0.0.1:8500'
-        }
+          node.normal['consul_template'] = {
+            consul: '127.0.0.1:8500'
+          }
 
-        include_recipe 'consul-template::default'
+          include_recipe 'consul-template::default'
 
-        consul_template_config "#{new_resource.app_name}_env" do
-          templates [{
-            source: "/var/www/#{new_resource.app_name}/shared/.env.ctmpl",
-            destination: "/var/www/#{new_resource.app_name}/shared/.env"
-          }]
+          consul_template_config "#{new_resource.app_name}_env" do
+            templates [{
+              source: "/var/www/#{new_resource.app_name}/shared/.env.ctmpl",
+              destination: "/var/www/#{new_resource.app_name}/shared/.env"
+            }]
+          end
         end
       end
 
--- libraries/resource_wordpress_cluster_app.rb
+++ libraries/resource_wordpress_cluster_app.rb
@@ -8,8 +8,11 @@ class Chef
       default_action :create
 
       attribute :app_name, kind_of: String, name_attribute: true
+      attribute :deployment_user, kind_of: String, default: 'deploy'
+      attribute :deployment_group, kind_of: String, default: 'deploy'
       attribute :server_name, kind_of: String, default: nil
       attribute :server_aliases, kind_of: Array, default: []
+      attribute :development, equal_to: [true, false], default: false
     end
   end
 end
