0a6286ce7a2a4ca87bdef0e76217ef393c7bc2ea
Adam Krone <krone.adam@gmail.com>
2015-01-28 13:35:22 -0600

    Removes csync2 and lsyncd

--- libraries/resource_wordpress_cluster_app.rb
+++ libraries/resource_wordpress_cluster_app.rb
@@ -14,9 +14,6 @@ class Chef
       attribute :server_aliases, kind_of: Array, default: []
       attribute :scm, equal_to: ['git', 'hg'], required: true
       attribute :github_accounts, kind_of: Array, required: true
-      attribute :csync2_hosts, kind_of: Array, default: nil
-      attribute :csync2_key, kind_of: String, default: nil
-      attribute :lsyncd_watched_dirs, kind_of: Array, default: nil
       attribute :development, equal_to: [true, false], default: false
       attribute :consul_servers, kind_of: Array
       attribute :consul_bind_interface, kind_of: String, default: nil
