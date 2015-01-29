4e4e5a0f47a0e4e4bb439a337163e27261af4dd7
Adam Krone <krone.adam@gmail.com>
2015-01-29 17:04:48 -0600

    WIP: switching to percona cluster
    Still very rough. Had to revert to using execute to create the user
    and db, as the database cookbook has some issues with percona.

--- libraries/resource_wordpress_cluster_db.rb
+++ libraries/resource_wordpress_cluster_db.rb
@@ -4,7 +4,7 @@ class Chef
   class Resource
     class WordpressClusterDb < Chef::Resource::LWRPBase
       self.resource_name = :wordpress_cluster_db
-      actions :create, :delete
+      actions :create
       default_action :create
 
       attribute :environment, kind_of: String, name_attribute: true
@@ -15,8 +15,20 @@ class Chef
       attribute :mysql_root_password, kind_of: String, required: true
       attribute :development, equal_to: [true, false], default: false
       attribute :consul_servers, kind_of: Array
-      attribute :consul_bind_interface, kind_of: String, default: nil
+      attribute :consul_bind_interface, kind_of: String, required: true
       attribute :datacenter, kind_of: String
+      attribute :node_ips, kind_of: Array, default: []
+
+      def node_ip
+        node['network']['interfaces']["#{consul_bind_interface}"]['addresses']
+          .detect{|k,v| v['family'] == 'inet'}
+          .first
+      end
+
+      def first_node?
+        return true if node_ip == node_ips.first
+        false
+      end
     end
   end
 end
