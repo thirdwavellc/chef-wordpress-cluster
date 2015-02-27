0a12b98d42576fc0b82d797ea1d2f447ac730655
Adam Krone <krone.adam@gmail.com>
2015-02-27 09:26:35 -0600

    v1.0.0 release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'adam.krone@thirdwavellc.com'
 license          'Apache v2.0'
 description      'Installs/Configures a high availability wordpress cluster'
 long_description 'Installs/Configures a high availability wordpress cluster'
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'apt'
 depends 'yum'
