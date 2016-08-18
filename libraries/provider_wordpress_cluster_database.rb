df5e40ba4d573933f3b7225d568a02b44bd49dd3
Adam Krone <krone.adam@gmail.com>
2016-08-18 15:58:25 -0500

    Adds 'provides' information for LWRPs

--- libraries/provider_wordpress_cluster_database.rb
+++ libraries/provider_wordpress_cluster_database.rb
@@ -25,6 +25,7 @@ class Chef
     class WordpressClusterDatabase < Chef::Provider::LWRPBase
       include Chef::DSL::IncludeRecipe
       use_inline_resources if defined?(use_inline_resources)
+      provides :wordpress_cluster_database
 
       def whyrun_supported?
         true
